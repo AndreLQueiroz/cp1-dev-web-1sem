1d88a85e424be48f62a3115a65ac84438ff4a111
Andre <andrezicadobaile1@gmail.com>
2024-03-26 19:33:33 -0300

    adicionando trabalho de javascript

---
+++ script.js
@@ -0,0 +1,43 @@
+//ex 1-Feito por André
+//Variavel indefinida
+let indefinido;
+//Variavel vazia
+let vazio = null;
+//Varivavel com valor
+let valor = 95;
+//Mostrando a saida das variaveis
+console.log (indefinido);
+console.log(vazio);
+console.log(valor);
+
+
+
+
+//ex 2-feito por André
+//Usando operadores 
+// Verificar se a variavel é maior ou igual ao valor de comparação
+let x = 3;
+let y = 10;
+if(x >= y){
+    console.log("X é maior ou igual que Y");
+}else{
+    console.log("X é menor que Y");
+}
+
+// Verificar se a variavel é diferente do numero de comparação
+let a = 5;
+let b = 10;
+if(a != b){
+    console.log("A é diferente de B");
+}else{
+    console.log("A é igual a B");
+}
+
+// Verificar se a variavel é ou não igual ao numero de comparação
+let p = 20;
+let o = 20;
+if(p == 0){
+    console.log("P é igual a O");
+}else{
+    console.log("P é diferente de O")
+}
